b59271c7f2c41356caef91695e4bcef9ecaa5d40
Dave Snowdon <dave@davesnowdon.com>
2018-03-24 17:40:21 +0000

    Encapsulate each action in a class and add movement to classroom routine

--- app/src/main/java/com/example/android/pepperpals/RoutineActivity.java
+++ app/src/main/java/com/example/android/pepperpals/RoutineActivity.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.aldebaran.qi.Function;
+import com.aldebaran.qi.Future;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.GoToBuilder;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.builder.TransformBuilder;
+import com.aldebaran.qi.sdk.object.actuation.Actuation;
+import com.aldebaran.qi.sdk.object.actuation.Frame;
+import com.aldebaran.qi.sdk.object.actuation.FreeFrame;
+import com.aldebaran.qi.sdk.object.actuation.GoTo;
+import com.aldebaran.qi.sdk.object.actuation.Mapping;
 import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.object.geometry.Transform;
 
 public class RoutineActivity extends AppCompatActivity implements RobotLifecycleCallbacks {
 
     private static final String TAG = RoutineActivity.class.getSimpleName();
+
+    private static final int CLASSROOM_ROUTINE = 0;
+
+    Routine routines[];
 
     int[] routineIds;
 
@@ -33,18 +47,14 @@ public class RoutineActivity extends AppCompatActivity implements RobotLifecycle
         ApplicationState globalState = (ApplicationState) getApplication();
         Log.i(TAG, "Currently at " + globalState.getCurrentRoutine() + " in our routine");
 
-        routineIds = new int[]{R.drawable.routine_school, R.drawable.routine_math};
-        routineWords = new String[]{
-                "Let's go to the classroom",
-                "Maths is fun. Let's listen to the teacher"
-        };
+        routines = new Routine[]{new ClassroomRoutine(), new MathsRoutine()};
 
-        if (globalState.getCurrentRoutine() >= routineIds.length) {
-            globalState.setCurrentRoutine(routineIds.length - 1);
+        if (globalState.getCurrentRoutine() >= routines.length) {
+            globalState.setCurrentRoutine(routines.length - 1);
         }
 
         routineView = (ImageView) findViewById(R.id.image_routine);
-        routineView.setImageResource(routineIds[globalState.getCurrentRoutine()]);
+        routineView.setImageResource(routines[globalState.getCurrentRoutine()].getImageResourceId());
     }
 
     @Override
@@ -58,7 +68,7 @@ public class RoutineActivity extends AppCompatActivity implements RobotLifecycle
     public void onRobotFocusGained(QiContext qiContext) {
         Log.d(TAG, "Gained robot focus");
         ApplicationState globalState = (ApplicationState) getApplication();
-        doRoutine(qiContext, globalState.getCurrentRoutine());
+        routines[globalState.getCurrentRoutine()].run(qiContext);
     }
 
     @Override
@@ -70,12 +80,74 @@ public class RoutineActivity extends AppCompatActivity implements RobotLifecycle
     public void onRobotFocusRefused(String reason) {
         Log.d(TAG, "Robot focus refused");
     }
-
-    private void doRoutine(QiContext qiContext, int routineNum) {
-        String wordsToSpeak = routineWords[routineNum];
-        Say say = SayBuilder.with(qiContext)
-                .withText(wordsToSpeak)
+    
+    public static Say buildSay(QiContext qiContext, String text) {
+        return SayBuilder.with(qiContext)
+                .withText(text)
                 .build();
-        say.async().run();
+    }
+
+    public interface Routine {
+        int getImageResourceId();
+
+        void run(final QiContext qiContext);
+    }
+
+    private static class ClassroomRoutine implements Routine {
+
+        private static final String CLASSROOM_SPEECH = "Let's go to the classroom";
+
+        @Override
+        public int getImageResourceId() {
+            return R.drawable.routine_school;
+        }
+
+        @Override
+        public void run(final QiContext qiContext) {
+            Future<Void> sayFuture = RoutineActivity.buildSay(qiContext, CLASSROOM_SPEECH).async().run();
+
+            // Chain a function to the future.
+            sayFuture.andThenCompose(new Function<Void, Future<Void>>() {
+                @Override
+                public Future<Void> execute(Void ignore) throws Throwable {
+                    return move(qiContext);
+                }
+            });
+        }
+
+        private Future<Void> move(QiContext qiContext) {
+            // get robot frame
+            Actuation actuation = qiContext.getActuation();
+            Frame robotFrame = actuation.robotFrame();
+
+            // create transform
+            Transform transform = TransformBuilder.create().fromXTranslation(1);
+
+            // create destination
+            Mapping mapping = qiContext.getMapping();
+            FreeFrame destination = mapping.makeFreeFrame();
+            destination.update(robotFrame, transform, System.currentTimeMillis());
+
+            GoTo goTo = GoToBuilder.with(qiContext)
+                    .withFrame(destination.frame())
+                    .build();
+
+            return goTo.async().run();
+        }
+    }
+
+    private static class MathsRoutine implements Routine {
+
+        private static final String MATHS_SPEECH = "Maths is fun. Let's listen to the teacher";
+
+        @Override
+        public int getImageResourceId() {
+            return R.drawable.routine_math;
+        }
+
+        @Override
+        public void run(QiContext qiContext) {
+            RoutineActivity.buildSay(qiContext, MATHS_SPEECH).async().run();
+        }
     }
 }
